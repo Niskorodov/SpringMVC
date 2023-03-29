68f24abd88a1dd94c10b69460ce5ee81ecc95c85
Dmitrii <103382610+Niskorodov@users.noreply.github.com>
2023-03-29 19:55:27 +0300

    commit

--- src/main/java/ru/niskorodov/springcourse/HelloController.java
+++ src/main/java/ru/niskorodov/springcourse/HelloController.java
@@ -1,2 +1,12 @@
-package ru.niskorodov.springcourse;public class HelloController {
+package ru.niskorodov.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
 }
